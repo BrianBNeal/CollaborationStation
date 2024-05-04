28a945f582ae116a740fceaca13ba3ad5b417da9
Brian Neal <brianbneal@gmail.com>
2024-05-04 07:40:22 -0500

    Add project files.

---
+++ CollaborationStation/CollaborationStation/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CollaborationStation.Hubs;
+
+public class ChatHub : Hub<IChatHubClient>
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.ReceiveMessage(user, message);
+    }
+}
+
+public interface IChatHubClient
+{
+    public Task ReceiveMessage(string user, string message);
+}
